afbdc99fd352e78498f829069fba9656af7d014b
Danie Xie <daniel.xie@farfetch.com>
2018-01-05 22:05:51 +0800

    MVP version

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+const constants = {
+  API_HOST: 'https://api.danielxie.cn'
+}
+
+export default constants
---
+++ src/utils/parser.js
@@ -0,0 +1,44 @@
+const supportTags = [
+  'a', 'abbr', 'b', 'blockquote', 'br', 'code', 'col', 'colgroup', 'dd', 'del', 'div', 'dl', 'dt', 'em', 'fieldset', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'img', 'ins', 'label', 'legend', 'li', 'ol', 'p', 'q', 'span', 'strong', 'sub', 'sup', 'table', 'tbody', 'td', 'tfoot', 'th', 'thead', 'tr', 'ul'
+]
+
+const styleList = {
+  div: 'font-size:14px;line-height:1.8;',
+  p: 'font-size:14px;line-height:1.8;',
+  h2: 'font-size:18px;line-height:1.8;margin:0.4rem 0;',
+  h3: 'font-size:16px;line-height:1.8;margin:0.4rem 0;',
+  h4: 'font-size:15px;line-height:1.8;margin:0.3rem 0;',
+  pre: 'white-space:pre;background-color:#f8f8f8;line-height:1.2;padding:0.4rem;margin:0.4rem 0;overflow-x:auto;-webkit-overflow-scrolling: touch;',
+  code: 'font-family:Menlo,Monaco,Consolas,Courier New,monospace;font-size:10px;padding:.26rem .53em;color:#4e5980;background-color:#f8f8f8;border-radius:2px;line-height:1.2;',
+  blockquote: 'margin:1em 0;border-left:4px solid #ddd;padding:0.4rem 0.6rem;color:#666;background-color:#f8f8f8;',
+  img: 'display:block;width:100%;margin:1rem 0;',
+  hr: 'margin:2rem 0;'
+}
+
+export function parser(html) {
+  const reg = new RegExp('<(/?([a-zA-Z1-9]+)) ?[^>]*>', 'ig')
+  const parsedHtml = html.replace(reg, ($0, $1) => {
+    const tag = $1.replace(/\//ig, '')
+    if (supportTags.indexOf(tag) >= 0) {
+      if (tag === 'img') {
+        const src = $0.match(/ src="([^"]+)"/ig)
+        if ($1.indexOf('/') >= 0) {
+          return `<${$1}>`
+        } else {
+          return `<${$1}${src} style="${styleList[tag] || ''}">`
+        }
+      }
+      if ($1.indexOf('/') >= 0) {
+        return `<${$1}>`
+      } else {
+        return `<${$1} style="${styleList[tag] || ''}">`
+      }
+    } else if ($1.indexOf('/') >= 0) {
+      return `<${$1.replace(/[a-zA-Z]+/ig, 'div')}>`
+    } else {
+      return `<${$1.replace(/[a-zA-Z]+/ig, 'div')} style="${styleList[tag] || ''}">`
+    }
+  })
+  console.log(parsedHtml)
+  return parsedHtml
+}
